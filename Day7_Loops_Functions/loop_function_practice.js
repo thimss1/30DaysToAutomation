c0c85e4a20cbe7e5689c8365c62442ffdb36a3ea
thimss1 <ssmithga@gmail.com>
2025-11-04 21:08:30 -0600

    FIx: Correct template literals in Day7 code

--- Day7_Loops_Functions/loop_function_practice.js
+++ Day7_Loops_Functions/loop_function_practice.js
@@ -1,17 +1,19 @@
 const features = ["Login", "Signup", "Checkout"];
+
 function testFeature(feature) {
-    console.log('🔍 Starting ${feature} test...');
-   
-    let passed = false;
-    let attempt = 0;
-   
-    while (!passed && attempt <3) {
-        console.log(`Attempt ${attempt + 1} for ${feature}`);
-        attempt++;
-        passed = Math.random() >0.5;
-    }
-    console.log(passed ? `✅ ${feature} passed` : `❌ ${feature} failed after retries`);
-    console.log("-------");
+  console.log(`🔍 Starting ${feature} test...`);
+
+  let passed = false;
+  let attempt = 0;
+
+  while (!passed && attempt < 3) {
+    console.log(`Attempt ${attempt + 1} for ${feature}`);
+    attempt++;
+    passed = Math.random() > 0.5;
+  }
+
+  console.log(passed ? `✅ ${feature} passed` : `❌ ${feature} failed after retries`);
+  console.log("---------");
 }
 
 features.forEach(testFeature);
